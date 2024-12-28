a0bb21288bbbe0494130c95c4c3788c40df74456
Leo Gaskin <leo.gaskin@le0.gs>
2024-12-28 06:26:46 +0100

    Whooo

--- src/main.zig
+++ src/main.zig
@@ -18,67 +18,112 @@ fn bufSetGreaterThan(_: void, lhs: std.BufSet, rhs: std.BufSet) bool {
     return lhs.count() > rhs.count();
 }
 
-fn generatePerfectHash(allocator: std.mem.Allocator, keys: []const []const u8) !void {
-    const e = 2;
-    const m = keys.len * (1 + e);
-    const r = std.math.sqrt(m);
+fn bufSetIndexGreaterThan(sets: []const std.BufSet, lhs: usize, rhs: usize) bool {
+    return sets[lhs].count() > sets[rhs].count();
+}
 
-    const buckets = try allocator.alloc(std.BufSet, r);
-    defer allocator.free(buckets);
-    for (buckets) |*bucket| {
-        bucket.* = std.BufSet.init(allocator);
-    }
+pub const PerfectHash = struct {
+    E: []u8,
+    m: usize,
+    allocator: std.mem.Allocator,
 
-    for (keys) |key| {
-        const i = Hash.hash(0, key) % r;
-        try buckets[i].insert(key);
-    }
+    pub fn build(allocator: std.mem.Allocator, keys: []const []const u8) !PerfectHash {
+        const e = 0;
+        const m = keys.len * (1 + e);
+        const r = std.math.sqrt(m);
 
-    std.mem.sort(std.BufSet, buckets, {}, bufSetGreaterThan);
+        const buckets = try allocator.alloc(std.BufSet, r);
+        defer allocator.free(buckets);
+        for (buckets) |*bucket| {
+            bucket.* = std.BufSet.init(allocator);
+        }
 
-    var E = try allocator.alloc(u8, buckets.len);
-    @memset(E, 0);
-    defer allocator.free(E);
+        const indices = try allocator.alloc(usize, r);
+        defer allocator.free(indices);
+        for (keys) |key| {
+            const i = Hash.hash(0, key) % r;
+            try buckets[i].insert(key);
+            indices[i] = i;
+        }
 
-    var T = try allocator.alloc(bool, m);
-    @memset(T, false);
-    defer allocator.free(T);
+        std.mem.sort(usize, indices, buckets, bufSetIndexGreaterThan);
+        std.mem.sort(std.BufSet, buckets, {}, bufSetGreaterThan);
 
-    var K = try allocator.alloc(u32, keys.len);
-    defer allocator.free(K);
+        var E = try allocator.alloc(u8, buckets.len);
+        @memset(E, 0);
 
-    for (0.., buckets) |i, *B| {
-        defer B.deinit();
-        l_loop: for (1..std.math.maxInt(u8) + 1) |l| {
-            @memset(K, 0);
-            var B_iter = B.iterator();
-            while (B_iter.next()) |elem| {
-                var D = Hash.init(0);
-                D.update(elem.*);
-                D.update(&[_]u8{@intCast(l)});
-                const hash = D.final();
+        var T = try allocator.alloc(bool, m);
+        @memset(T, false);
+        defer allocator.free(T);
 
-                if (T[hash % T.len]) {
-                    continue :l_loop;
+        var K = try allocator.alloc(u32, keys.len);
+        defer allocator.free(K);
+
+        for (indices, buckets) |i, *B| {
+            defer B.deinit();
+            l_loop: for (1..std.math.maxInt(u8) + 1) |l| {
+                @memset(K, 0);
+                var B_iter = B.iterator();
+                while (B_iter.next()) |elem| {
+                    var D = Hash.init(0);
+                    D.update(elem.*);
+                    D.update(&[_]u8{@intCast(l)});
+                    const final = D.final();
+
+                    if (T[final % T.len]) {
+                        continue :l_loop;
+                    }
+                    K[final % K.len] = final;
                 }
-                K[hash % K.len] = hash;
-            }
 
-            if (!std.mem.containsAtLeast(u32, K, K.len - B.count() + 1, &.{0})) {
-                E[i] = @intCast(l);
-                for (K) |j| {
-                    T[j % T.len] = true;
+                if (!std.mem.containsAtLeast(u32, K, K.len - B.count() + 1, &.{0})) {
+                    E[i] = @intCast(l);
+                    for (K) |j| {
+                        if (j != 0) {
+                            T[j % T.len] = true;
+                        }
+                    }
+                    break;
+                } else if (l >= std.math.maxInt(u8)) {
+                    return error.PerfectHashTimeout;
                 }
-                break;
             }
         }
+
+        return .{
+            .allocator = allocator,
+            .E = E,
+            .m = m,
+        };
     }
-}
+
+    pub fn deinit(self: *PerfectHash) void {
+        self.allocator.free(self.E);
+    }
+
+    pub fn hash(self: *PerfectHash, key: []const u8) !u32 {
+        var acc = Hash.init(0);
+        acc.update(key);
+        const l = self.E[acc.final() % self.E.len];
+        acc.update(&[_]u8{l});
+
+        return @intCast(acc.final() % self.m);
+    }
+
+    // pub fn unhash(self: *PerfectHash, value: u32) anyerror![]const u8 {
+    //     return self.T[value];
+    // }
+};
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer std.debug.assert(gpa.deinit() == .ok);
 
-    try generatePerfectHash(allocator, &example_keys);
+    var ph = try PerfectHash.build(allocator, &example_keys);
+    defer ph.deinit();
+
+    for (example_keys) |key| {
+        std.debug.print("{s} = {d}\n", .{ key, try ph.hash(key) });
+    }
 }
