10dd14ec5d9d9dbf10ba7fd8276563d480c49ee5
Leo Gaskin <leo.gaskin@le0.gs>
2025-01-03 00:16:02 +0100

    Commit initial build config

---
+++ build.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "aligntools",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    b.installArtifact(exe);
+
+    {
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| run_cmd.addArgs(args);
+
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
+    }
+
+    {
+        const main_test = b.addTest(.{
+            .root_source_file = b.path("src/test.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        const test_run = b.addRunArtifact(main_test);
+
+        const test_step = b.step("test", "Run all tests");
+        test_step.dependOn(&test_run.step);
+    }
+}
