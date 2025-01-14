364b0d811213a570b3b2d4597a506dad7e0c55ef
Leo Gaskin <leo.gaskin@le0.gs>
2025-01-14 01:30:13 +0100

    Cleanup test code, remove bench for now

--- src/test.zig
+++ src/test.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const testing = std.testing;
 const PerfectHash = @import("perfect_hash.zig").PerfectHash;
 const hirschberg_gotoh = @import("hirschberg_gotoh.zig");
 
@@ -15,36 +16,23 @@ const example_keys: [10][]const u8 = .{
     "robot",
 };
 
-test "old main" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer std.debug.assert(gpa.deinit() == .ok);
-
-    var ph = try PerfectHash.build(allocator, &example_keys);
+test "test perfect hash" {
+    var ph = try PerfectHash.build(testing.allocator, &example_keys);
     defer ph.deinit();
 
     for (example_keys) |key| {
         std.debug.print("{s} => {d}\n", .{ key, ph.hash(key) });
     }
+}
 
+test "test cost" {
     const A = "dddddadcd";
     const B = "0adcd";
 
-    const cost = try hirschberg_gotoh.cost(allocator, A, B);
+    const cost = try hirschberg_gotoh.cost(testing.allocator, A, B);
     std.debug.print("cost: {d}\n", .{cost});
 
-    const edits = try hirschberg_gotoh.transform(allocator, A, B);
+    const edits = try hirschberg_gotoh.transform(testing.allocator, A, B);
     defer edits.deinit();
-    try hirschberg_gotoh.write_edits(std.io.getStdErr().writer(), edits.items);
+    try hirschberg_gotoh.writeEdits(std.io.getStdErr().writer(), edits.items);
 }
-
-// test "bench" {
-//     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-//     const allocator = gpa.allocator();
-//     defer std.debug.assert(gpa.deinit() == .ok);
-
-//     const start = try std.time.Instant.now();
-//     const edits_ = try hirschberg_gotoh.transform(allocator, "a" ** (1 << 18), "b" ** (1 << 18));
-//     defer edits_.deinit();
-//     std.debug.print("Elapsed: {s}\n", .{std.fmt.fmtDuration((try std.time.Instant.now()).since(start))});
-// }
